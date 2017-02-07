a445ab4af3a5a4dcc37dadbd666d76b3e3c73eb8
Chaojin <jinchaoonly@gmail.com>
2017-02-07 16:50:33 -0500

    comment the codes

--- DreamTeam/Assets/Resources/Scripts/GameWorld/GameManager.cs
+++ DreamTeam/Assets/Resources/Scripts/GameWorld/GameManager.cs
@@ -2,21 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/***************************************************************************************************/
+/*       Gamemanager: control camera switch                                                        */
+/*                  start();                                                                       */
+/*                  update();                                                                      */
+/*                  toggleCamera();                                                                */
+/*                                                                                                 */
+/***************************************************************************************************/
 public class GameManager : MonoBehaviour {
 
-	public static GameManager gm;
+	//    Public Variables
+	public static GameManager gm;					//Refernece to  the gamemanager
 
-	public GameObject firstPersonCamera;
-	public GameObject thirdPersonCamera;
+	public GameObject firstPersonCamera;			//Refernece to  the firstpersoncamera
+	public GameObject thirdPersonCamera;			//Refernece to  the thirdpersoncamera
 
-	public bool thirdPersonActive;
+	public bool thirdPersonActive;					////Refernece to whether the thirdpersoncamera is active
 
 	// Use this for initialization
 	void Start () {
+
+		//if gm is undifined, get the refernce to GameManger
 		if (gm == null) {
-			gm = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<GameManager> ();
+			gm = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<GameManager> ();  
 		}
 
+		//initial thirdpersoncamera inactive
 		thirdPersonActive = false;
 		
 	}
@@ -24,12 +35,16 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		//if left shift key is down, set the active of thirdpersoncamera the opposite 
 		if (Input.GetKeyDown(KeyCode.LeftShift)){
 			thirdPersonActive = !thirdPersonActive;
+
+			//call toggleCamera function
 			toggleCamera (thirdPersonActive);
 		}
 	}
 
+	//the function we set the active/inactive of firstperson and thirdperson camera
 	void toggleCamera(bool b){
 		if (b){
 			// make camera view thirdperson camera
