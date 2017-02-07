e18d85416f82867f9cf11749d97a803cda2b99c3
Chaojin <jinchaoonly@gmail.com>
2017-02-06 21:18:49 -0500

    Add firstpersoncontroller

---
+++ DreamTeam/Assets/Resources/Scripts/Player/FirstPersonController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour {
+
+	//declare the variables we need
+	public bool isjumping;
+	public bool iswalking;
+	public float movespeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxis ("Horizontal");
+		float z = Input.GetAxis ("Vertical");
+
+		//isjumping = Input.GetAxis("Jump");
+
+		PlayerMove (x,z);
+	}
+
+	//This is the function makes player moving.
+	void PlayerMove(float x, float z){
+
+		transform.position = new Vector3 (transform.position.x + x * movespeed * Time.deltaTime,
+			transform.position.y, transform.position.z + z * movespeed * Time.deltaTime);
+		
+	}
+}
