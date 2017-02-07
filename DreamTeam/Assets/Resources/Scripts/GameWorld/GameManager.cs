de58e21682ffe7b1b25454ae57340dcb7360fce7
chjwilliams <chrsjwilliams@gmail.com>
2017-02-06 23:33:24 -0500

    Started GameManagerScript
    Currently working on swtching between cameras

---
+++ DreamTeam/Assets/Resources/Scripts/GameWorld/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager gm;
+
+	public GameObject firstPersonCamera;
+	public GameObject thirdPersonCamera;
+
+	public bool thirdPersonActive;
+
+	// Use this for initialization
+	void Start () {
+		if (gm == null) {
+			gm = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<GameManager> ();
+		}
+
+		thirdPersonActive = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.LeftShift)){
+			thirdPersonActive = !thirdPersonActive;
+			toggleCamera (thirdPersonActive);
+		}
+	}
+
+	void toggleCamera(bool b){
+		if (b){
+			// make camera view thirdperson camera
+			thirdPersonCamera.SetActive(true);
+			firstPersonCamera.SetActive (false);
+		} 
+		else {
+			//	make camera view first person camera
+			thirdPersonCamera.SetActive(false);
+			firstPersonCamera.SetActive(true);
+		}
+	}
+}
