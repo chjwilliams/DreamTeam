96dde6e9a501b9a520e6faa4b08becb9d086ed16
chjwilliams <chrsjwilliams@gmail.com>
2017-02-07 18:06:02 -0500

    Basic NPC Interaction Done. Basic HARTO Interface Done
    Player can now interact with NPC and "communicate" with them via HARTO

--- DreamTeam/Assets/Resources/Scripts/NPC/BasicNPCController.cs
+++ DreamTeam/Assets/Resources/Scripts/NPC/BasicNPCController.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class BasicNPCController : MonoBehaviour {
 
+	public HartoTuningController HARTO;
+	public float myFrequency;
+	public float range;
+	public bool acknowledgePlayer;
+
 
 	// Use this for initialization
 	void Start () {
+		HARTO = GameObject.FindGameObjectWithTag ("HARTO").GetComponent<HartoTuningController> ();
+		myFrequency = 60;
+		range = 2.5f;
+		acknowledgePlayer = false;
 		
 	}
 	
@@ -15,7 +24,22 @@ public class BasicNPCController : MonoBehaviour {
 		
 	}
 
-	void OnTriggerEnter(Collider other) {
-		transform.Translate (Vector3.up);
+	void OnTriggerStay(Collider other) {
+		if (other.gameObject.CompareTag ("Player")) {
+			if (!acknowledgePlayer) {
+				transform.Translate (Vector3.up);
+				acknowledgePlayer = true;
+			}
+			if (HARTO.currentfrequency > myFrequency - range && HARTO.currentfrequency < myFrequency + range) {
+				GetComponent<MeshRenderer> ().material.color = new Color (0.0f, 0.0f, 1.0f);
+			}
+			else {
+				GetComponent<MeshRenderer> ().material.color = new Color (1.0f, 0.0f, 0.0f);
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		acknowledgePlayer = false;
 	}
 }
--- DreamTeam/Assets/Resources/Scripts/Player/HartoTuningController.cs
+++ DreamTeam/Assets/Resources/Scripts/Player/HartoTuningController.cs
@@ -6,55 +6,107 @@ public class HartoTuningController : MonoBehaviour {
 
 	//	Public variables
 	public bool HARTOisActive;
+	public float HARTOalpha;
+	public float incrementAlpha = 2.0f;
 	public float movespeed = 5.0f;
 	public float currentfrequency;
 	public float frequencyincrement = 5.0f;
 	public KeyCode increasefrequency = KeyCode.Alpha1;
 	public KeyCode decreasefrequency = KeyCode.Alpha2;
 	public KeyCode activateHARTO = KeyCode.Tab;
-
+	public GameObject HARTOKnotch;
+	public GameObject HARTOKnob;
 
 	//	Private variables
 	private Transform _transform;
+	private MeshRenderer _meshRenderer;
+	private MeshRenderer _KnotchMeshRenderer;
+	private MeshRenderer _KnobMeshRenderer;
 
 	// Use this for initialization
 	void Start () {
 		_transform = GameObject.FindGameObjectWithTag ("HartoKnob").transform;
 		currentfrequency = _transform.localRotation.eulerAngles.y;
 		HARTOisActive = false;
+		_meshRenderer = GetComponent<MeshRenderer> ();
+		_KnotchMeshRenderer = HARTOKnotch.GetComponent<MeshRenderer> ();
+		_KnobMeshRenderer = HARTOKnob.GetComponent<MeshRenderer> ();
+		_meshRenderer.material.color = new Color(_meshRenderer.material.color.r, 
+													_meshRenderer.material.color.g, 
+													_meshRenderer.material.color.b, 
+													0.0f);
+		_KnotchMeshRenderer.material.color = new Color(_KnotchMeshRenderer.material.color.r, 
+														_KnotchMeshRenderer.material.color.g, 
+														_KnotchMeshRenderer.material.color.b, 
+														_meshRenderer.material.color.a);
+
+		_KnobMeshRenderer.material.color = new Color(_KnobMeshRenderer.material.color.r, 
+														_KnobMeshRenderer.material.color.g, 
+														_KnobMeshRenderer.material.color.b, 
+														_meshRenderer.material.color.a);
 	}
 		
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(decreasefrequency)) {
-			currentfrequency += frequencyincrement;
-			if (currentfrequency > 135.0f) {
-				currentfrequency = 135.0f;
+		if (!GameManager.gm.thirdPersonActive) {
+			if (Input.GetKey (decreasefrequency)) {
+				currentfrequency += frequencyincrement;
+				if (currentfrequency > 135.0f) {
+					currentfrequency = 135.0f;
+				}
+				_transform.localRotation = Quaternion.Euler (0, currentfrequency, 0);
+			} else if (Input.GetKey (increasefrequency)) {
+				currentfrequency -= frequencyincrement;
+				if (currentfrequency < -135.0f) {
+					currentfrequency = -135.0f;
+				}
+				_transform.localRotation = Quaternion.Euler (0, currentfrequency, 0);
 			}
-			_transform.localRotation = Quaternion.Euler (0, currentfrequency, 0);
-		}
-		else if (Input.GetKey(increasefrequency)) {
-			currentfrequency -= frequencyincrement;
-			if (currentfrequency < -135.0f) {
-				currentfrequency = -135.0f;
+
+			if (Input.GetKeyDown (activateHARTO)) {
+				HARTOisActive = !HARTOisActive;
+
 			}
-			_transform.localRotation = Quaternion.Euler (0, currentfrequency, 0);
-		}
 
-		if (Input.GetKey(activateHARTO)) {
-			HARTOisActive = !HARTOisActive;
-			toggleHARTO (HARTOisActive);
-		}
+			if (HARTOisActive) {
+				HARTOalpha += incrementAlpha * Time.deltaTime;
+				if (HARTOalpha > 1.0f) {
+					HARTOalpha = 1.0f;
+				}
+			} else {
+				HARTOalpha -= incrementAlpha * Time.deltaTime;
+				if (HARTOalpha < 0.0f) {
+					HARTOalpha = 0.0f;
+				}
+			}
 			
-	}
-
-	void toggleHARTO (bool b){
-		if (b) {
-			
+			toggleHARTO (HARTOisActive, HARTOalpha);
 		}
 		else {
-			
+			toggleHARTO (HARTOisActive, 0);
+
 		}
+
+	}
+
+	void toggleHARTO (bool b, float alpha){
+		
+		_meshRenderer.material.color = new Color(_meshRenderer.material.color.r, 
+													_meshRenderer.material.color.g, 
+													_meshRenderer.material.color.b, 
+													alpha);
+		
+		_KnotchMeshRenderer.material.color = new Color(_KnotchMeshRenderer.material.color.r, 
+														_KnotchMeshRenderer.material.color.g, 
+														_KnotchMeshRenderer.material.color.b, 
+														_meshRenderer.material.color.a);
+
+		_KnobMeshRenderer.material.color = new Color(_KnobMeshRenderer.material.color.r, 
+														_KnobMeshRenderer.material.color.g, 
+														_KnobMeshRenderer.material.color.b, 
+														_meshRenderer.material.color.a);
+		
+
 	}
 
 	float getFrequency(){
