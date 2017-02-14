c233df07f676ce5d076a331eb2d69a5fd240ba56
chjwilliams <chrsjwilliams@gmail.com>
2017-02-13 21:09:00 -0500

    HARTOTuningController Commented

--- DreamTeam/Assets/Scripts/Player/HartoTuningController.cs
+++ DreamTeam/Assets/Scripts/Player/HartoTuningController.cs
@@ -2,51 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*--------------------------------------------------------------------------------------------------*/
+/*                                                                                                 	*/
+/*       HartoTuningController: controls functionality of HARTO                                    	*/
+/*             	void Start()                                                                        */
+/*				void UpdateHARTOButtons(KeyCode key 												*/
+/*             	IEnumerator FadeIn ()                                                              	*/
+/*             	void Update()                                                                     	*/
+/*             	void toggleHARTO (float alpha)                                                     	*/
+/*              void setHARTOActiveTo(bool canUse, bool canTune, bool canEmote)                   	*/
+/*              void defaultHARTOEmotions()                                                       	*/
+/*              bool canUseHARTO()                                                                 	*/
+/*              bool canTuneHARTO()                                                               	*/
+/*              bool canEmoteHARTO()                                                              	*/
+/*                                                                                                 	*/
+/*--------------------------------------------------------------------------------------------------*/
 public class HartoTuningController : MonoBehaviour {
 
 
 	//	Public variables
-	public bool HARTOisActive;
-	public bool isHappy;
-	public bool isCurious;
-	public bool isSad;
-	public float HARTOalpha;
-	public float incrementAlpha = 2.0f;
-	public float movespeed = 5.0f;
-	public float currentfrequency;
-	public float frequencyincrement = 5.0f;
-	public KeyCode increasefrequency = KeyCode.Alpha1;
-	public KeyCode decreasefrequency = KeyCode.Alpha2;
-	public KeyCode activateHARTO = KeyCode.Tab;
-	public KeyCode setHappy = KeyCode.Z;
-	public KeyCode setCurious = KeyCode.X;
-	public KeyCode setSad = KeyCode.C;
-	public GameObject HARTOKnotch;
-	public GameObject HARTOKnob;
-	public MeshRenderer happyButton;
-	public MeshRenderer curiousButton;
-	public MeshRenderer sadButton;
+	public bool HARTOisActive;									//	Communicates to other GameObjects that HARTO is active
+	public bool isHappy;										//	Is HARTO set to happy
+	public bool isCurious;										//	Is HARTO set to Curious
+	public bool isSad;											//	Is HARTO set to Sad
+	public float HARTOalpha;									//	The Alpha of Harto GameObject
+	public float incrementAlpha = 2.0f;							//	How much we increment/decrement the alpha channel of HARTO GameObject
+	public float movespeed = 5.0f;								//	TBD
+	public float currentfrequency;								//	Stores current frequency of HARTO
+	public float frequencyincrement = 5.0f;						//	How much you increment the frequncy of HARTO with each press/hold
+	public KeyCode increasefrequency = KeyCode.Alpha1;			//	Button to increase frequency
+	public KeyCode decreasefrequency = KeyCode.Alpha2;			//	Button to decrease frequency
+	public KeyCode activateHARTO = KeyCode.Tab;					//	Button to active HARTO
+	public KeyCode setHappy = KeyCode.Z;						//	Button to Set HARTO to Happy
+	public KeyCode setCurious = KeyCode.X;						//	Button to set HARTO to Curious
+	public KeyCode setSad = KeyCode.C;							//	Button to set HARTO to Sad
+	public GameObject HARTOKnotch;								//	Reference to the knoch on the knob
+	public GameObject HARTOKnob;								//	Reference to the Knob on HARTO
+	public MeshRenderer happyButton;							//	Reference to the Happy Button GameObject
+	public MeshRenderer curiousButton;							//	Referecne to the Curious Button GameObject
+	public MeshRenderer sadButton;								//	Referecne to the Sad Button GameObject
 
 	//	Private variables
-	[SerializeField] private bool m_CanUseHARTO;
-	[SerializeField] private bool m_CanEmoteHARTO;
-	[SerializeField] private bool m_CanTuneHARTO;
-	private Transform _transform;
-	private MeshRenderer _meshRenderer;
-	private MeshRenderer _KnotchMeshRenderer;
-	private MeshRenderer _KnobMeshRenderer;
+	[SerializeField] private bool m_CanUseHARTO;				//	Tells us if we can or cannot use HARTO
+	[SerializeField] private bool m_CanEmoteHARTO;				//	Tells us if we can select an emotion with HARTO
+	[SerializeField] private bool m_CanTuneHARTO;				//	Tells us if we can tune HARTO
+	private Transform _transform;								//	Reference to GameObject's Transform
+	private MeshRenderer _meshRenderer;							//	Reference to GameObject's MeshRenderer
+	private MeshRenderer _KnotchMeshRenderer;					//	Reference to the Knotch's MeshRenderer
+	private MeshRenderer _KnobMeshRenderer;						//	Reference to Knob's MeshRenderer
 
-	// Use this for initialization
+	/*--------------------------------------------------------------------------------------*/
+    /*																						*/
+    /*	Start: Runs once at the begining of the game. Initalizes variables.					*/
+    /*																						*/
+    /*--------------------------------------------------------------------------------------*/
 	void Start () {
+		//	Sets up transform's reference
 		_transform = GameObject.FindGameObjectWithTag ("HartoKnob").transform;
+
+		//	Sets the current frequency
 		currentfrequency = _transform.localRotation.eulerAngles.y;
+
+		//	HARTO starts game not active
 		HARTOisActive = false;
+
+		//	Sets up the references to the MeshRenderers
 		_meshRenderer = GetComponent<MeshRenderer> ();
 		_KnotchMeshRenderer = HARTOKnotch.GetComponent<MeshRenderer> ();
 		_KnobMeshRenderer = HARTOKnob.GetComponent<MeshRenderer> ();
 		happyButton = GameObject.Find("Happy").GetComponent<MeshRenderer>();
 		curiousButton = GameObject.Find("Curious").GetComponent<MeshRenderer>();
 		sadButton = GameObject.Find("Sad").GetComponent<MeshRenderer>();
+
+		//	Makes HARTO GameObject conglomerate transparent
 		_meshRenderer.material.color = new Color(_meshRenderer.material.color.r, 
 													_meshRenderer.material.color.g, 
 													_meshRenderer.material.color.b, 
@@ -60,13 +88,22 @@ public class HartoTuningController : MonoBehaviour {
 														_KnobMeshRenderer.material.color.g, 
 														_KnobMeshRenderer.material.color.b, 
 														_meshRenderer.material.color.a);
+		
+		//	HARTO functionality turned off until activated in Prototype
 		m_CanUseHARTO = false;
      	m_CanEmoteHARTO = false;
      	m_CanTuneHARTO =  false;
 
-		 defaultHARTOEmotions();
+		//	Defeaults all emotions to off
+		defaultHARTOEmotions();
 	}
 
+	/*--------------------------------------------------------------------------------------*/
+    /*																						*/
+    /*	UpdateHARTOButtons: Changes and displays the active emotion 						*/
+    /*		param: KeyCode key - the Key associated with each emotion						*/
+	/*																						*/
+    /*--------------------------------------------------------------------------------------*/
 	public void UpdateHARTOButtons(KeyCode key) {
 			if (key == setHappy) {
 				isHappy = true;
@@ -102,7 +139,11 @@ public class HartoTuningController : MonoBehaviour {
 			}
 	}
 
-	// Update is called once per frame
+	/*--------------------------------------------------------------------------------------*/
+    /*																						*/
+    /*	Update: Called once per frame														*/
+    /*																						*/
+    /*--------------------------------------------------------------------------------------*/
 	void Update () {
 		if (!GameManager.gm.thirdPersonActive && m_CanUseHARTO) {
 			
@@ -128,10 +169,6 @@ public class HartoTuningController : MonoBehaviour {
 					_transform.localRotation = Quaternion.Euler (0, currentfrequency, 0);
 				}
 			}
-
-			
-
-			
 
 			if (Input.GetKeyDown (activateHARTO)) {
 				HARTOisActive = !HARTOisActive;
@@ -159,6 +196,12 @@ public class HartoTuningController : MonoBehaviour {
 
 	}
 
+	/*--------------------------------------------------------------------------------------*/
+    /*																						*/
+    /*	toggleHARTO: Hides and displays HARTO												*/
+    /*		param: float alpha - the alpha channel value for the MeshRenderer				*/
+	/*																						*/
+    /*--------------------------------------------------------------------------------------*/
 	void toggleHARTO (float alpha){
 		
 		_meshRenderer.material.color = new Color(_meshRenderer.material.color.r, 
@@ -190,28 +233,54 @@ public class HartoTuningController : MonoBehaviour {
 
 	}
 
-
+	/*--------------------------------------------------------------------------------------*/
+    /*																						*/
+    /*	setHARTOActiveTo: Toggles functionality of HARTO									*/
+    /*		param: bool canUse - can HARTO be activated										*/
+	/*		param: bool canTune - can HARTO be tuned										*/
+	/*		param: bool canEmote - can HARTO emotions buttons be used						*/
+	/*																						*/
+    /*--------------------------------------------------------------------------------------*/
 	public void setHARTOActiveTo(bool canUse, bool canTune, bool canEmote){
 		m_CanUseHARTO = canUse;
 		m_CanTuneHARTO = canTune;
 		m_CanEmoteHARTO = canEmote;
 	}
 
-	public void defaultHARTOEmotions()
-	{
+	/*--------------------------------------------------------------------------------------*/
+    /*																						*/
+    /*	defaultHARTOEmotions: turns all HARTO Emotions off									*/
+    /*																						*/
+    /*--------------------------------------------------------------------------------------*/
+	public void defaultHARTOEmotions() {
 		isHappy = false;
 		isCurious = false;
 		isSad = false;
 	}
 
+	/*--------------------------------------------------------------------------------------*/
+    /*																						*/
+    /*	canUseHARTO: returns true if we can use HARTO										*/
+    /*																						*/
+    /*--------------------------------------------------------------------------------------*/
 	public bool canUseHARTO(){
 		return m_CanUseHARTO;
 	}
 
+	/*--------------------------------------------------------------------------------------*/
+    /*																						*/
+    /*	canTuneHARTO: returns true if we can tune HARTO										*/
+    /*																						*/
+    /*--------------------------------------------------------------------------------------*/
 	public bool canTuneHARTO(){
 		return m_CanTuneHARTO;
 	}
 
+	/*--------------------------------------------------------------------------------------*/
+    /*																						*/
+    /*	canEmoteHARTO: returns true if we can use HARTO	 emotion buttons					*/
+    /*																						*/
+    /*--------------------------------------------------------------------------------------*/
 	public bool canEmoteHARTO(){
 		return m_CanEmoteHARTO;
 	}
