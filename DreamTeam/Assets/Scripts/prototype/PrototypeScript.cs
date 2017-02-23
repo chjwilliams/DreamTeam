9f5b460dcdcf3ebc7a6a660e7d7c11d523acd055
Chaojin <jinchaoonly@gmail.com>
2017-02-23 11:57:46 -0600

    Dialogue Tree step 0

---
+++ DreamTeam/Assets/Scripts/prototype/PrototypeScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrototypeScript : MonoBehaviour {
+
+	public GameObject momResponses;
+	public GameObject myResponses;
+
+	// Use this for initialization
+	void Start () {
+
+		momResponses.SetActive (true);
+		myResponses.SetActive (true);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+	}
+}
